5c753f4e3a82e04f9c1d4e8f12994564869881dd
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-09-26 10:52:30 +0200

    Delete ejercicio8.py

--- ejercicio8.py
+++
@@ -1,7 +0,0 @@
-num1 = float(input("Introduce un número:"))
-if num1 == 0 :
-    print ("El número es 0")
-elif num1 > 0 :
-    print ("El número es positivo")
-else :
-    print ("El número es negativo")
