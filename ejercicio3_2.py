5283e31934787bf5e2f825de65a6fcd52054f4a7
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-09-26 10:51:08 +0200

    Delete ejercicio3_2.py

--- ejercicio3_2.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Introduce tu edad:"))
-pais = input("Eres de este país:")
-if num1 >= 18 and pais == "si" or "Si" :
-    print("Puedes votar")
-else :
-    print("No puedes votar")
